186a491d0f22043c459b4d15c17d509b083fb650
Zhijie Li <ZhijieLi@outlook.com>
2017-12-04 23:13:11 +0800

    add BST

---
+++ TestSample/Interview/BinarySearchTree.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestSample.Interview
+{
+    class BinarySearchTree<T> : BinaryTree<T> where T : IComparable<T>
+    {
+        public void Insert(T data)
+        {
+            if (Root == null)
+            {
+                Root = new BinTreeNode<T> { Data = data };
+            }
+
+            BinTreeNode<T> current = Root;
+            BinTreeNode<T> parent =null;
+
+            while (current != null)
+            {
+                if (data.CompareTo(current.Data) >= 0)
+                {
+                    parent = current;
+                    current = current.RightChild;
+                }
+                else
+                {
+                    parent = current;
+                    current = current.LeftChild;
+                }
+            }
+        }
+    }
+}
---
+++ TestSample/Interview/BinaryTree.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestSample.Interview
+{
+    public class BinTreeNode<T>
+    {
+        public BinTreeNode<T> LeftChild;
+        public BinTreeNode<T> RightChild;
+        public T Data;
+    }
+
+    public class BinaryTree<T>
+    {
+        public BinTreeNode<T> Root { get; set; }
+
+        public int Height { get; }
+
+        public void PreVisit(Action<T> action)
+        {
+            BinTreeNode<T> current = Root;
+            var stack = new Stack<BinTreeNode<T>>();
+
+            while (current != null || stack.Count > 0)
+            {
+                while (current != null)
+                {
+                    action(current.Data);
+                    stack.Push(current);
+                    current = current.LeftChild;
+                }
+
+                current = stack.Pop();
+                current = current.RightChild;
+                while (current == null)
+                {
+                    current = stack.Pop();
+                    current = current.RightChild;
+                }
+            }
+        }
+
+        public void InVisit() { }
+    }
+}
